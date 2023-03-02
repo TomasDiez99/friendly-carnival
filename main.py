805d9a8d54586bda4fe83edbe2ac3f5c5eb835be
Tomás Joaquín Diez <dieztomasjoaquin@gmail.com>
2023-03-02 15:52:50 -0300

    pipeline workflow

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+"""
+Este módulo crea un archivo HTML de ejemplo en la carpeta 'build'
+"""
+
 import os
 
 if not os.path.exists('build'):
     os.makedirs('build')
 
-with open('build/index.html', 'w') as f:
+with open('build/index.html', 'w', encoding='utf-8') as f:
     f.write('<html><body><h1>Testing pipeline</h1></body></html>')
