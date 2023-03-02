3bcd9b972dbf1e33ce897ed39560575c164c252b
Tomás Joaquín Diez <dieztomasjoaquin@gmail.com>
2023-03-02 14:44:29 -0300

    update script

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import os
 
-def main():
-    if not os.path.exists('build'):
-        os.makedirs('build')
-        
-    with open('build/index.html', 'w') as f:
-        f.write('<html><body><h1>Testing pipeline</h1></body></html>')
+if not os.path.exists('build'):
+    os.makedirs('build')
 
-if __name__ == '__main__':
-    main()
+with open('build/index.html', 'w') as f:
+    f.write('<html><body><h1>Testing pipeline</h1></body></html>')
