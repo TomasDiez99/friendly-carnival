a4635b3f4875acdd5d11ca2a1f7ad2aaadcda338
Tomás Joaquín Diez <dieztomasjoaquin@gmail.com>
2023-03-02 16:17:58 -0300

    upd6

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ Este módulo crea un archivo HTML de ejemplo en la carpeta 'build'
 
 import os
 
+
 def main():
     if not os.path.exists('build'):
         os.makedirs('build')
 
     with open('build/index.html', 'w', encoding='utf-8') as f:
         f.write('<html><body><h1>Testing pipeline</h1></body></html>')
+
 
 if __name__ == '__main__':
     main()
